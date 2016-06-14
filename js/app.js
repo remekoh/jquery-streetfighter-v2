1cd6ae234030246c371cc9f2dd4b4d03bc18267d
Sean Jun <seanjun21@gmail.com>
2016-06-14 05:34:05 -0700

    Fixed app.js

--- js/app.js
+++ js/app.js
@@ -2,15 +2,21 @@ $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
+		$('.ryu-throwing').hide();
+		$('.ryu-cool').hide();
 	})
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+		$('.ryu-throwing').hide();
+		$('.ryu-cool').hide();
 	})
 	.mousedown(function() {
 		playHadouken()
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
+		$('.ryu-still').hide();
+		$('.ryu-cool').hide();
 		$('.hadouken').finish().show().animate(
 		  {'left': '1020px'},
 		  500,
@@ -23,8 +29,25 @@ $(document).ready(function() {
 	.mouseup(function() {
 		$('.ryu-ready').show();
 		$('.ryu-throwing').hide();
+		$('.ryu-cool').hide();
+		$('.ryu-still').hide();
 	});
+}).keydown(function() {
+	if (keyCode = 88) {
+        $('.ryu-ready').hide();
+        $('.ryu-cool').show();
+        $('.ryu-still').hide();
+        $('.ryu-throwing').hide();
+    } 
+}).keyup(function() {
+    if (keyCode = 88) {
+        $('.ryu-cool').hide();
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').hide();
+    }
 });
+
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
