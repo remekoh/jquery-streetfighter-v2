fdbae26b611fb7a9b6c96a82bfdbfd19d1824a9b
rem{e}koh <git@remekoh.me>
2016-06-13 22:10:29 -0500

    Added Text

--- js/app.js
+++ js/app.js
@@ -2,21 +2,15 @@ $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
-		$('.ryu-cool').hide();
-		$('.ryu-throwing').hide();
 	})
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
-		$('.ryu-cool').hide();
-		$('.ryu-throwing').hide();
 	})
 	.mousedown(function() {
-		playHadouken();
+		playHadouken()
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.ryu-cool').hide();
-		$('.ryu-still').hide();
 		$('.hadouken').finish().show().animate(
 		  {'left': '1020px'},
 		  500,
@@ -27,28 +21,10 @@ $(document).ready(function() {
 		);
 	})
 	.mouseup(function() {
-		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-		$('.ryu-cool').hide();
-		$('.ryu-still').hide();
-	})
-})
-.keydown(function(e){
-	if (e.keyCode == 88) {
-		$('.ryu-still').hide();
-		$('.ryu-cool').show();
-		$('.ryu-ready').hide();
 		$('.ryu-throwing').hide();
-	}
-}).keyup(function(e){
-	if (e.keyCode == 88) {
-		$('.ryu-still').show();
-		$('.ryu-cool').hide();
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').hide();
-	}
+	});
 });
-
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
